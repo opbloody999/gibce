f2a1ef96c0194d4831adf8ecb4449a377ee1290b
opbloody999 <opbloody999@gmail.com>
2025-09-23 13:22:46 +0500

    Update CoursesSection.tsx

--- client/src/components/CoursesSection.tsx
+++ client/src/components/CoursesSection.tsx
@@ -85,7 +85,7 @@ export default function CoursesSection({ onCourseSelect }: CoursesSectionProps) 
           </p>
         </div>
 
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {courses.map((course) => (
             <Card 
               key={course.id} 
